f9eae25eb7041cb5c74f2e74d3112df3e9277344
Tim <timminata@gmail.com>
2017-03-01 23:16:16 +0200

    First commit

--- TestMap/Form1.Designer.cs
+++ TestMap/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCollectorFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 27);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 22;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(970, 660);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 6D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(974, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCollectorFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openCollectorFileToolStripMenuItem
+            // 
+            this.openCollectorFileToolStripMenuItem.Name = "openCollectorFileToolStripMenuItem";
+            this.openCollectorFileToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.openCollectorFileToolStripMenuItem.Text = "Open Collector File";
+            this.openCollectorFileToolStripMenuItem.Click += new System.EventHandler(this.openCollectorFileToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 688);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCollectorFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- TestMap/Form1.cs
+++ TestMap/Form1.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using System.IO;
+using Newtonsoft.Json;
+using GMap.NET.WindowsForms.Markers;
 
 namespace TestMap
 {
     public partial class Form1 : Form
     {
+        GMapOverlay overlay = new GMapOverlay();
+
         public Form1()
         {
             InitializeComponent();
+            gMapControl1.Overlays.Add(overlay);
         }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(-33, 18);
+        }
+
+        private void openCollectorFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader r = new StreamReader(openFileDialog1.FileName))
+                {
+                    string json = r.ReadToEnd();
+                    dynamic track = JsonConvert.DeserializeObject(json);
+                    List<PointLatLng> allPoints = new List<PointLatLng>();
+                    int i = 0;
+                    foreach(var point in track.points)
+                    {
+                        var pointLatLng = new PointLatLng(Convert.ToDouble(point.latitude), Convert.ToDouble(point.longitude));
+                        GMarkerGoogle marker = new GMarkerGoogle(pointLatLng, GMarkerGoogleType.gray_small);
+                        if (Convert.ToDouble(point.accuracy) > 20) marker = new GMarkerGoogle(pointLatLng, GMarkerGoogleType.red_small);
+                        marker.ToolTipText = ""+i++;
+                        overlay.Markers.Add(marker);
+                        allPoints.Add(pointLatLng);
+                    }
+                    GMapRoute route = new GMapRoute(allPoints, "test");
+                    overlay.Routes.Add(route);
+                    gMapControl1.ZoomAndCenterRoute(route);
+                }
+            }
+        }
+
+        
     }
 }
